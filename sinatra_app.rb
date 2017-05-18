2e3b00296c752cb3632458bf2bdf0a2d67d477b0
Andrew H Yi <andrew.yi50@gmail.com>
2017-05-18 17:48:43 -0400

    make it work on heroku

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -2,12 +2,15 @@ require "sinatra"
 require_relative "lib/meat"
 
 class SinatraApp < Sinatra::Base
+
+  CHAR_LIMIT = 10_000
+
   get "/" do
     erb :index
   end
 
   post "/process" do
-    s = StoryMaker.new(text: params[:text], save_to_file: false)
+    s = StoryMaker.new(text: params[:text].to_s[0..CHAR_LIMIT], save_to_file: false)
     @html = s.compile!
   end
 end
